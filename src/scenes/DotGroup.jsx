0d41b22b2f6b17be461cbcd0f00e1bd5fbd02150
Sreehari <95231762+vr-sreehari@users.noreply.github.com>
2023-01-14 22:45:06 +0530

    removed dot for testimonials
    <AnchorLink
            className={`${
              selectedPage === "testimonials" ? selectedStyles : "bg-dark-grey"
            }
          w-3 h-3 rounded-full`}
            href={`#testimonials`}
            onClick={() => setSelectedPage("testimonials")}
          />

--- src/scenes/DotGroup.jsx
+++ src/scenes/DotGroup.jsx
@@ -30,14 +30,6 @@ const DotGroup = ({ selectedPage, setSelectedPage }) => {
       />
       <AnchorLink
         className={`${
-          selectedPage === "testimonials" ? selectedStyles : "bg-dark-grey"
-        }
-      w-3 h-3 rounded-full`}
-        href={`#testimonials`}
-        onClick={() => setSelectedPage("testimonials")}
-      />
-      <AnchorLink
-        className={`${
           selectedPage === "contact" ? selectedStyles : "bg-dark-grey"
         }
       w-3 h-3 rounded-full`}
